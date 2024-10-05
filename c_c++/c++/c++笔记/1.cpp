d72030942f3de4dbf506f7db8830a02c7af8ffc3
小狗正版授权 <1193220313@stu.jiangnan.edu.cn>
2024-10-05 10:26:38 +0800

    Add files via upload

---
+++ c_c++/c++/c++笔记/1.cpp
@@ -0,0 +1,12 @@
+//Helloworld.cpp   --显示一条消息
+ 
+#include <iostream>
+ 
+int main() {                    //函数头
+      //函数体
+    using namespace std;        //using 编译指令
+    cout << "Hello, World!"    //输出“Hello,World!”
+         << endl;              //换行
+ 
+    return 0;                  //返回语句，结束main()函数
+}
